ea1eb0b80e5286ae1bbb86f6519c56a8d92c9791
fnska <mraero17@mail.ru>
2020-08-20 20:38:37 +0300

    ui to api v2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,14 @@
 var app = angular.module("bookingApp", []);
 
 app.controller("dateCtrl", function($scope, $http) {
-    $http.get('http://localhost:8080/api/v1/reservations').then(function(response) {
+    $http.get('http://localhost:8080/api/v2/reservations').then(function(response) {
         $scope.reservations = response.data;
     });
 });
 
 app.controller("customerCtrl", function($scope, $http) {
     $scope.getCustomers = function(date) {
-        $http.get('http://localhost:8080/api/v1/customers/search?date=' + date).then(function(response) {
+        $http.get('http://localhost:8080/api/v2/customers/search?date=' + date).then(function(response) {
             $scope.customers = response.data;
         });
     }
